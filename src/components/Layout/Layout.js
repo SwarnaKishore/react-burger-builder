fe909186153ca142c4c5d585e7b4ac8cb7d0a80f
SwarnaKishore <swarnakishoree@gmail.com>
2021-02-09 16:42:00 -0600

    updated burger builder

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Aux from '../../HOC/Aux';
+import classes from './Layout.css';
 
 const layout = (props) => (
     <Aux>
-         <div>layout</div>
-        <main>{props.children}</main>
+        <div>layout</div>
+        <main className={classes.Content}>
+            {props.children}
+        </main>
     </Aux> 
 );
 
