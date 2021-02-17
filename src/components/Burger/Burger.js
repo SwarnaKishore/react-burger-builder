8938495e173fbe8e55ddd5f815382989d6397ab1
SwarnaKishore <swarnakishoree@gmail.com>
2021-02-17 07:25:45 -0600

    calculate burger ingredient dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,15 @@ import './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
  
 const burger = (props) => {
+    const transformedIngredients = Object.keys( props.ingredients ).map((ingredientKey) => {
+        return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
+          return <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />
+        });
+    });
     return (
         <div className="Burger">
             <BurgerIngredient type="bread-top"></BurgerIngredient>
-            <BurgerIngredient type="cheese"></BurgerIngredient>
-            <BurgerIngredient type="meat"></BurgerIngredient>
+           {transformedIngredients}
             <BurgerIngredient type="bread-bottom"></BurgerIngredient>
         </div>
     );
