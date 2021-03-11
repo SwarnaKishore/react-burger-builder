1296144ae4091fc9766d0b667be245e3c30dc562
SwarnaKishore <swarnakishoree@gmail.com>
2021-03-10 21:02:18 -0600

    adding image to source folder

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Logo from '../../Logo/Logo';
 import classes from './Toolbar.module.css';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
+        <Logo height='80%'/>
         <div>MENU</div>
         <div>LOGO</div>
         <nav>
